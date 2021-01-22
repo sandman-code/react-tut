1bf9e302be55a1d57fcaab1b82aceb5d29965d0a
Caden Crist <cade3217@gmail.com>
2021-01-22 18:12:17 -0500

    updateed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let books = [
+    {
+     id: 1,
+     img: 'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg',
+     title: 'A Promised Land',
+     author: 'Barack Obama'
+    },
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    {
+     id: 2,
+     img: 'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg',
+     title: 'A Promised Land',
+     author: 'Barack Obama'
+    }
+];
+function BookList() {
+    return (
+        <section>
+            {books.map( (book) => {
+                return <Book key = {book.id} {...book}/>;
+            })}
+        </section>
+    );
+}
+
+const Book = ({ img, title, author, children }) => {
+    return (
+        <article>
+            <img src={img} alt=""/>
+            <h3>{title}</h3>
+            <h4>{author}</h4>
+            {children}
+        </article>
+    );
+};
+
+ReactDOM.render(<BookList />,document.getElementById('root'));
